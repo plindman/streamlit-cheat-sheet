3ec5cdae9b7d004ed3a494296bb29f112f90f64a
Pontus Lindman <pontus.v.lindman@gmail.com>
2024-07-14 11:36:37 +0300

    New page structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("streamlit-cheat-sheet.py"), 
+pg = st.navigation([st.Page("streamlit-cheat-sheet.py"),
+                    st.Page("streamlit-sidebar-elements.py"), 
                     st.Page("streamlit-data-page.py")])
 pg.run()
